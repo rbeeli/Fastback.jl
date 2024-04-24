589b95d14463da986f3d611f014e161e38326dcd
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2024-04-24 16:48:43 +0200

    Add plots to random trading example

--- examples/random_trading.jl
+++ examples/random_trading.jl
@@ -19,11 +19,11 @@ DUMMY = Instrument(1, "DUMMY");
 instruments = [DUMMY];
 
 # create trading account
-acc = Account{Nothing}(instruments, 100_000.0);
+acc = Account{Nothing}(instruments, 10_000.0);
 
 # data collectors for account equity and drawdown
-collect_equity, equity_data = periodic_collector(Float64, Second(1));
-collect_drawdown, drawdown_data = drawdown_collector(DrawdownMode.Percentage, (v, dt, equity) -> dt - v.last_dt >= Second(1));
+collect_equity, equity_data = periodic_collector(Float64, Hour(1));
+collect_drawdown, drawdown_data = drawdown_collector(DrawdownMode.Percentage, (v, dt, equity) -> dt - v.last_dt >= Hour(1));
 
 # get position for instrument
 pos = get_position(acc, DUMMY);
@@ -35,7 +35,7 @@ for i in 1:N
 
     # randomly trade with 1% probability
     if rand() < 0.01
-        quantity = rand() > 0.5 ? 1.0 : -1.0
+        quantity = rand() > 0.4 ? 1.0 : -1.0
         order = Order(oid!(acc), DUMMY, dt, price, quantity)
         fill_order!(acc, order, dt, price; fill_quantity=0.75order.quantity, fees_pct=0.001)
     end
@@ -50,3 +50,12 @@ end
 
 # print account statistics
 show(acc)
+
+# -------------------------------
+
+# plot equity and drawdown
+using UnicodePlots, Term
+gridplot([
+    lineplot(dates(equity_data), values(equity_data); title="Account equity", height=12),
+    lineplot(dates(drawdown_data), 100values(drawdown_data); title="Account equity drawdowns [%]", color=:red, height=12)
+]; layout=(1, 2))
