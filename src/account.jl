bfddbe9f75186e911703aba7656d53692544bfe8
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2025-09-25 22:49:10 +0200

    rename hash_cash_symbol to has_cash_symbol + Account named params only

--- src/account.jl
+++ src/account.jl
@@ -12,10 +12,10 @@ mutable struct Account{OData,IData,CData}
     const date_format::Dates.DateFormat
 
     function Account(
+        ;
         date_format=dateformat"yyyy-mm-dd HH:MM:SS",
         order_sequence=0,
-        trade_sequence=0
-        ;
+        trade_sequence=0,
         odata::Type{OData}=Nothing,
         idata::Type{IData}=Nothing,
         cdata::Type{CData}=Nothing
@@ -49,7 +49,7 @@ they can be accessed, see `register_cash!`.
 """
 Checks if the account has the given cash symbol registered.
 """
-@inline hash_cash_symbol(acc::Account, symbol::Symbol) = haskey(acc.cash_by_symbol, symbol)
+@inline has_cash_symbol(acc::Account, symbol::Symbol) = haskey(acc.cash_by_symbol, symbol)
 
 """
 Registers a new cash asset in the account.
@@ -58,7 +58,7 @@ Cash is a liquid coin or currency that is used to trade instruments with, e.g. U
 When funding the account, the funds are added to the balance of the corresponding cash asset.
 """
 function register_cash!(acc::Account{OData,IData,CData}, cash::Cash{CData}) where {OData,IData,CData}
-    !hash_cash_symbol(acc, cash.symbol) || throw(ArgumentError("Cash with symbol '$(cash.symbol)' already registered."))
+    !has_cash_symbol(acc, cash.symbol) || throw(ArgumentError("Cash with symbol '$(cash.symbol)' already registered."))
 
     # set index for fast array indexing and hashing
     cash.index = length(acc.cash) + 1
@@ -79,7 +79,7 @@ To withdraw (subtract) cash from the account, simply pass a negative value.
 """
 function add_cash!(acc::Account{OData,IData,CData}, cash::Cash{CData}, value::Real) where {OData,IData,CData}
     # register cash object if not already registered
-    hash_cash_symbol(acc, cash.symbol) || register_cash!(acc, cash)
+    has_cash_symbol(acc, cash.symbol) || register_cash!(acc, cash)
 
     # ensure cash object was registered
     cash.index > 0 || throw(ArgumentError("Cash with symbol '$(cash.symbol)' not registered."))
